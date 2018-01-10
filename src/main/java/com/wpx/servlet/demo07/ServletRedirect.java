606fa4c09386b84901e68acfdadc343d4591d9da
wang125631 <1256317570@qq.com>
2018-01-10 21:15:34 +0800

    Signed-off-by: wang125631 <1256317570@qq.com>

---
+++ src/main/java/com/wpx/servlet/demo07/ServletRedirect.java
@@ -0,0 +1,32 @@
+package com.wpx.servlet.demo07;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 通过Servlet进行重定向
+ * 
+ * @author wangpx
+ */
+public class ServletRedirect extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//设置头
+		resp.setContentType("text/html;charset=utf-8");
+		//设置状态
+		resp.setStatus(302);
+		//要跳转的地址
+		resp.setHeader("location", "servlet/302.html");
+	}
+
+}
---
+++ src/main/webapp/servlet/login.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>用户登录模拟</title>
+</head>
+<body>
+<form action="servletLogin">
+	<table>
+	<tr>
+		<td>username:</td>
+		<td><input type="text" id="username" name="username" ></td>
+	</tr>
+	<tr>
+		<td>password:</td>
+		<td><input type="password" id="password" name="password"></td>
+	</tr>
+	<tr>
+		<td><input type="submit" value="登录"></td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
